7b25dedff5ef40117f47518d3c3bcec322cbd347
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2024-08-28 11:32:19 -0600

    Initial commit

---
+++ cliente/src/main/myp/files/main.cpp
@@ -0,0 +1,13 @@
+/*
+ *Primer proyecto de modelado y programación chatroom.
+ *Main del cliente.
+ *Autor: Patricio García Ocampo.
+ *Facultad de ciencias UNAM.
+ */
+
+#include <iostream>
+
+int main(){
+  std::cout<<"Este es el main del cliente"<<std::endl;
+  return 0;
+}
---
+++ servidor/src/main/myp/files/main.cpp
@@ -0,0 +1,13 @@
+/*
+ *Primer proyecto de modelado y programación chatroom.
+ *Main del servidor.
+ *Autor: Patricio García Ocampo.
+ *Facultad de ciencias UNAM.
+ */
+
+#include <iostream>
+
+int main(){
+  std::cout<<"Este es el main del servidor"<<std::endl;
+  return 0;
+}
