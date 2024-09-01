10f844f381c705c62fef2ce4b101e7a144a1c397
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2024-09-01 11:19:10 -0600

    Cambio de nombre de la enumeración y enum TipoServidor agregada

---
+++ cliente/src/main/myp/files/TipoCliente.h
@@ -0,0 +1,36 @@
+/*Tipo Cliente es una enumeración que contempla todas las solicitudes que un
+ *usuario puede hacer al servidor. Cada tipo está asociado a un número negativo
+ *con el fin de evitar posible conflictos con el código.
+ *Los números asociados son multipos de 10.
+ */
+#ifndef TIPOCLIENTE_H
+#define TIPOCLIENTE_H
+
+enum class TipoCliente{
+  //Se identifica el ussuario.
+  IDENTIFY = -10,
+  //Cambia el estatus del ususario.
+  STATUS = -20,
+  //Pide la lista de usuarios.
+  USERS = -30,
+  //Manda texto privado a un usuario,
+  TEXT = -40,
+  //Manda un mensaje publico.
+  PUBLIC_TEX = -50,
+  //Crea una nueva sala.
+  NEW_ROOM = -60;
+  //Invita a uno o más usuarios a una sala.
+  INVITE= -70,
+  //El usuario se une a una sala.
+  JOIN_ROOM = -80,
+  //Pide la lista de usuarios.
+  ROOM_USERS = -90,
+  //Manda un mensaje a la sala.
+  ROOM_TEXT = -100,
+  //Abandona una sala.
+  LEAVE_ROOM = -110,
+  //El usuario se desconecta.
+  DISCONNECT = -120
+};
+
+#endif 
---
+++ cliente/src/main/myp/files/TipoServidor.h
@@ -0,0 +1,34 @@
+/*La enumeración TipoServidor contempla todas las respuestas a las peticiones de
+ *los usuarios. El valor asociado a cada tipo es un múltipo de 5.
+ *Por cada petición de parte del usuario cuya enumeración es múltipo de 10, su
+ *respuesta será dicho valor más 5.
+ */
+#ifndef TIPOSERVIDOR_H
+#define TIPOSERVIDOR_H
+
+enum class TipoServidor{
+  //El cliente recibe una respuesta del servidor.
+  RESPONSE = -5,
+    //Un nuevo usuario se conectó.
+    NEW_USER = -15,
+    //Notifica a los clientes que un usuario cambió su estado.
+    NEW_STATUS = -25,
+    //Indica que se mandará la lista de ususarios.
+    USER_LIST = -35,
+    //Notifica al cliente que recibe un mensaje privado.
+    TEXT_FROM = -45,
+    //Notifica al cliente que un usuario mandó un texto público.
+    PUBLIC_TEXT_FROM = -55,
+    //Indica a los usuarios de la sala que un nuevo usuario se unió.
+    JOINED_ROOM = -85,
+    //Envía al usuario la lista de ususarios de la sala.
+    ROOM_USER_LIST = -95,
+    //El usuario recibe un dexto en un cuarto.
+    ROOM_TEXT_FROM = -105,
+    //Indica a los ususarios de la sala que otro usuario se fue.
+    LEFT_ROOM = -115,
+    //Indica a los usuarios que alguien se desconectó.
+    DISCONNECTED = -125
+};
+
+#endif
