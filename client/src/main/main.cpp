dd2a993ba9a8240bc450a9d05d2c10af6d416fab
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2024-12-18 20:33:14 -0600

    Se agregaron cosas en main

--- client/src/main/main.cpp
+++ client/src/main/main.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
 #include "./view/screen.h"
+#include "client.h"
+
+std::atomic<bool> continue_thread{true};
+
+void manageReceivedMessages(Screen sc){
+  while(continue_thread){
+    
+  }
+}
 
 int main(){
   const int maxSizeBuffer = 1024;
   Screen sc = Screen(maxSizeBuffer);
 
-  std::string port = sc.askAndGetPort();
+  int port = std::stoi(sc.askAndGetPort());
   std::string ip = sc.askAndGetIp();
 
-  while(true){
-    std::string userMessage = sc.getMessage();
+  // Se inicia la conexión con el servidor.
+  Client c = Client(port, maxSizeBuffer, ip);
 
-    std::cout<<"x: "<<userMessage<<std::endl;
+  try{
+    c.start();
+
+    std::thread thread_show_messages(manageReceivedMessages, sc);
+
+    while(continue_thread){
+      std::string userMessage = sc.getMessage();
+      if(userMessage == "x;"){
+	std::cout<<"byeee"<<std::endl;
+	continue_thread = false;
+      }
+    }
+    thread_show_messages.join();
+  }catch(ClientConnectionException &e){
+    char text[maxSizeBuffer];
+    strncpy(text, e.what(), maxSizeBuffer);
+    sc.showMessage("server", text, true, true);
   }
   
   return 0;
+  
 }
+
