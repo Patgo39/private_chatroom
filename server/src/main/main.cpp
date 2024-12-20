63f797b3e9e9af8d2d422766d07fb367991fac09
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2024-12-20 16:16:56 -0600

    Se implementan los métodos del servidor.

--- server/src/main/main.cpp
+++ server/src/main/main.cpp
@@ -2,5 +2,11 @@
 #include "server.h"
 
 int main(){
+  int port;
+  const int bufferSize = 1024;
+  std::cout<<"Type the port: ";
+  std::cin>>port;
+  Server s = Server(port, bufferSize);
+  s.start();
   return 0;
 }
