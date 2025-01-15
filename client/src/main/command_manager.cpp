9693faabbcf4c67c0f2ceffabe763a9f03c91ba6
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2025-01-14 22:29:44 -0600

    Se comienza a trabajar con jsons y los comandos del ususario

--- client/src/main/command_manager.cpp
+++ client/src/main/command_manager.cpp
@@ -7,8 +7,10 @@ CommandManager::CommandManager(){
 std::string CommandManager::getJsonFromCommand(std::string userMessage){
   std::vector<std::string> command = getCommandAsVector(userMessage);
 
-  if(command[0] == "REQ"){
+  if(command[0] == "/chat"){
     
+  }else{
+    return manageNormalText(userMessage);
   }
   
 }
@@ -22,4 +24,16 @@ std::vector<std::string> CommandManager::getCommandAsVector(std::string userMess
     command.push_back(temp);
   }
   return command;
+}
+
+std::string CommandManager::manageNormalText(std::string userMessage){
+  Json::Value json;
+  json["type"] = "PUBLIC_TEXT";
+  json["text"] = userMessage;
+}
+
+std::string CommandManager::turnJsonToString(Json::Value json){
+  Json::StreamWriterBuilder builder;
+  const std::string output = Json::writeString(builder, json);
+  return output;
 }
