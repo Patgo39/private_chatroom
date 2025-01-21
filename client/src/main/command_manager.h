56cb26f7f2b7371c559f0ff761da5b74ed90d75b
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2025-01-20 21:55:57 -0600

    Se agregan métodos para manejar los distintos comandos

--- client/src/main/command_manager.h
+++ client/src/main/command_manager.h
@@ -16,8 +16,17 @@ class CommandManager{
 
  private:
   std::vector<std::string> getCommandAsVector(std::string userMessage);
-
-  std::string manageNormalText(std::vector<std::string> command);
+  std::string manageIdentification(std::vector<std::string> command);
+  std::string manageNewStatus(std::vector<std::string> command);
+  std::string manageUsersList(std::vector<std::string> command);
+  std::string managePrivateMessage(std::vector<std::string> command);
+  std::string manageNewRoom(std::vector<std::string> command);
+  std::string manageInviteUsersToRoom(std::vector<std::string> command);
+  std::string manageJoinRoom(std::vector<std::string> command);
+  std::string manageRoomList(std::vector<std::string> command);
+  std::string manageLeaveRoom(std::vector<std::string> command);
+  std::string manageDisconnect(std::vector<std::string> command);
+  std::string manageNormalText(std::string userMessage);
 
   std::string turnJsonToString(Json::Value json);
 };
