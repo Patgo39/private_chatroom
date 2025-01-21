490b9b5459f06e059b876fc195fe4e13a0e668c2
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2025-01-20 21:55:57 -0600

    Se agregan métodos para manejar los distintos comandos

--- client/src/main/command_manager.cpp
+++ client/src/main/command_manager.cpp
@@ -7,7 +7,26 @@ CommandManager::CommandManager(){
 std::string CommandManager::getJsonFromCommand(std::string userMessage){
   std::vector<std::string> command = getCommandAsVector(userMessage);
 
-  if(command[0] == "/chat"){
+  if(command[0] == "/identify"){
+    return manageIdentification(command);
+    
+  }else if(command[0] == "/status"){
+    
+  }else if(command[0] == "/list"){
+    
+  }else if(command[0] == "/private"){
+    
+  }else if(command[0] == "/newroom"){
+    
+  }else if(command[0] == "/invite"){
+    
+  }else if(command[0] == "/join"){
+    
+  }else if(command[0] == "/roomlist"){
+    
+  }else if(command[0] == "/leave"){
+    
+  }else if(command[0] == "/disconnect"){
     
   }else{
     return manageNormalText(userMessage);
@@ -30,6 +49,8 @@ std::string CommandManager::manageNormalText(std::string userMessage){
   Json::Value json;
   json["type"] = "PUBLIC_TEXT";
   json["text"] = userMessage;
+
+  return turnJsonToString(json);
 }
 
 std::string CommandManager::turnJsonToString(Json::Value json){
@@ -37,3 +58,44 @@ std::string CommandManager::turnJsonToString(Json::Value json){
   const std::string output = Json::writeString(builder, json);
   return output;
 }
+
+std::string CommandManager::manageIdentification(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageNewStatus(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageUsersList(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::managePrivateMessage(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageNewRoom(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageInviteUsersToRoom(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageJoinRoom(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageRoomList(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageLeaveRoom(std::vector<std::string> command){
+
+}
+
+std::string CommandManager::manageDisconnect(std::vector<std::string> command){
+
+}
+
