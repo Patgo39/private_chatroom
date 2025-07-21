38f875448ffd411fa2584e6b58f648e5be9f1167
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2025-07-20 23:45:22 -0600

    Se documentó el json controller

--- server/src/main/json_controller.cpp
+++ server/src/main/json_controller.cpp
@@ -4,6 +4,12 @@ JsonController::JsonController(){
 }
 
 /**Errores de formato*/
+
+/**
+ * Se construye el json para notificar al cliente que el formato del json que envió es
+ * incorrecto.
+ * @return json construido convertido a cadena.
+ */
 std::string JsonController::getInvalidJsonResponse(){
   Json::Value json;
   json["type"] = "RESPONSE";
@@ -13,6 +19,10 @@ std::string JsonController::getInvalidJsonResponse(){
   return turnJsonToString(json);
 }
 
+/**
+ * Construye el json para notificar al cliente que aún no se ha identificado.
+ * @return json el json construido convertido a cadena.
+ */
 std::string JsonController::getNotIdentifiedResponse(){
   Json::Value json;
   json["type"] = "RESPONSE";
@@ -22,13 +32,48 @@ std::string JsonController::getNotIdentifiedResponse(){
   return turnJsonToString(json);
 }
 
-std::string JsonController::turnJsonToString(Json::Value json){
-  Json::StreamWriterBuilder builder;
-  const std::string output = Json::writeString(builder, json);
-  return output;
+/**
+ * Se construye el json que notifica al cliente si su nombre de usuario supera los 8
+ * caracteres.
+ * @param username el nombre de usuario que incumple con el formato requerido.
+ * @return el json construido convertido a cadena.
+ */
+std::string JsonController::getUsernameMaxCharResponse(std::string username){
+  Json::Value json;
+  
+  json["type"] = "RESPONSE";
+  json["operation"] = "IDENTIFY";
+  json["result"] = "NAME_TOO_LONG";
+  json["extra"] = username;
+
+  return turnJsonToString(json);
+}
+
+/**
+ * Se construye el json que notifica al cliente que el nombre de cuarto proporcionado para
+ * crear un cuarto supera los 16 caracteres.
+ * @param roomName el nombre de cuarto que incumple el formato requerido.
+ * @return el json convertido a cadena.
+ */
+std::string JsonController::getNewRoomMaxCharResponse(std::string roomName){
+  Json::Value json;
+  
+  json["type"] = "RESPONSE";
+  json["operation"] = "NEW_ROOM";
+  json["result"] = "NAME_TOO_LONG";
+  json["extra"] = roomName;
+
+  return turnJsonToString(json);
 }
 
 /**Respuestas para el solicitante de la petición*/
+
+/**
+ * Se construye el json que notifica al cliente que su petición de identificación fue
+ * exitosa.
+ * @param username el nombre de usuario del cliente que se identificó correctamente.
+ * @return el json construido a cadena.
+ */
 std::string JsonController::getIdentificationResponse(std::string username){
   Json::Value json;
 
@@ -40,6 +85,12 @@ std::string JsonController::getIdentificationResponse(std::string username){
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json que notifica al cliente si el nombre de usuario que proporcionó
+ * para identificarse ya está siendo ocupado por otro cliente conectado.
+ * @param username el nombre de usuario ocupado.
+ * @return el json contruido a cadena.
+ */
 std::string JsonController::getUsernameAlreadyExistsResponse(std::string username){
   Json::Value json;
   
@@ -51,17 +102,11 @@ std::string JsonController::getUsernameAlreadyExistsResponse(std::string usernam
   return turnJsonToString(json);
 }
 
-std::string JsonController::getUsernameMaxCharResponse(std::string username){
-  Json::Value json;
-  
-  json["type"] = "RESPONSE";
-  json["operation"] = "IDENTIFY";
-  json["result"] = "USERNAME_TOO_LONG";
-  json["extra"] = username;
-
-  return turnJsonToString(json);
-}
-
+/**
+ * Se construye el json con la lista de usuarios conectados que el cliente solicitó.
+ * @param userMap el mapa de usuarios del servidor.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getServerUserListResponse(std::map<int, Client> usersMap){
   Json::Value json;
   Json::Value users;
@@ -78,29 +123,74 @@ std::string JsonController::getServerUserListResponse(std::map<int, Client> user
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json que notifica al cliente que su petición de crear un nuevo cuarto
+ * fue exitosa.
+ * @param roomName el nombre del cuarto creado por el usuario.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getNewRoomCreatedResponse(std::string roomName){
-  return "";
+  Json::Value json;
+  json["type"] = "RESPONSE";
+  json["operation"] = "NEW_ROOM";
+  json["result"] = "SUCCESS";
+  json["extra"] = roomName;
+  
+  return turnJsonToString(json);
 }
 
-std::string JsonController::getNewRoomMaxCharResponse(std::string roomName){
+/**
+ * Se construye el json para notificar al cliente que el nombre de cuarto que solicita
+ * crear ya existe.
+ * @param roomName el nombre del cuarto que ya existe.
+ * @return el json convertido a cadena.
+ */
+std::string JsonController::getRoomAlreadyExistsResponse(std::string roomName){
   Json::Value json;
   
   json["type"] = "RESPONSE";
-  json["operation"] = "IDENTIFY";
-  json["result"] = "ROOMNAME_TOO_LONG";
+  json["operation"] = "NEW_ROOM";
+  json["result"] = "ROOM_ALREADY_EXISTS";
   json["extra"] = roomName;
 
   return turnJsonToString(json);
 }
 
-std::string JsonController::getRoomAlreadyExistsResponse(std::string roomName){
-  return "";
-}
- 
+/**
+ * Se construye el json para notificar al cliente que el cuarto en el que intenta hacer
+ * alguna solicitud no existe. Las operaciones válidas para esta función son: INVITE,
+ * JOIN_ROOM, ROOM_USERS, ROOM_TEXT, LEAVE_ROOM.
+ * @param roomName el nombre del cuarto que no existe.
+ * @param type el tipo de operación que el cliente solicitó.
+ * @return el json convertido a cadena.
+ * @throw InvalidOperationException si la función recibe una operación inválida.
+ */
 std::string JsonController::getNoSuchRoomResponse(std::string roomName, Operation::Type type){
-  return "";
+  Json::Value json;
+
+  if(type == Operation::Type::TEXT){
+    throw InvalidOperationException("");
+  }
+
+  std::string operation = Operation::getStringFromType(type);
+  
+  json["type"] = "RESPONSE";
+  json["operation"] = operation;
+  json["result"] = "NO_SUCH_ROOM";
+  json["extra"] = roomName;
+
+  return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json para notificar al cliente que el usuario que invitó a un cuarto o
+ * le envió un mensaje privado no existe. Las operaciones válidas para esta función son:
+ * TEXT, INVITE.
+ * @param username el nombre de usuario del cliente que no existe.
+ * @param type el tipo de operación que el cliente solicitó.
+ * @return el json convertido a cadena.
+ * @throw InvalidOperationException si la función recibe una operación inválida.
+ */
 std::string JsonController::getNoSuchUserResponse(std::string username, Operation::Type type){
 
   std::string operation;
@@ -108,7 +198,7 @@ std::string JsonController::getNoSuchUserResponse(std::string username, Operatio
   switch(type){
   case Operation::Type::INVITE: operation = "INVITE"; break;
   case Operation::Type::TEXT: operation = "TEXT"; break;
-  default: /**Lanza error*/break;
+  default: throw InvalidOperationException("");
   }
   
   Json::Value json;
@@ -121,24 +211,103 @@ std::string JsonController::getNoSuchUserResponse(std::string username, Operatio
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json para notificar que un cliente se unió exitosamente
+ * a un cuarto.
+ * @param roomName el nombre del cuarto al que el cliente se unió.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getJoinRoomSuccessResponse(std::string roomName){
-  return "";
+  Json::Value json;
+  
+  json["type"] = "RESPONSE";
+  json["operation"] = "JOIN_ROOM";
+  json["result"] = "SUCCESS";
+  json["extra"] = roomName;
+
+  return turnJsonToString(json);
 }
 
-std::string JsonController::getNotInvitedResponse(std::string roomName, Operation::Type type){
-  return "";
+/**
+ * Se construye el json que notifica al cliente que aún no ha sido invitado
+ * a un cuarto.
+ * @param roomName el nombre del cuarto al que el cliente no se ha unido.
+ * @return el json convertido a cadena.
+ */
+std::string JsonController::getNotInvitedResponse(std::string roomName){
+  Json::Value json;
+  
+  json["type"] = "RESPONSE";
+  json["operation"] = "JOIN_ROOM";
+  json["result"] = "NOT_INVITED";
+  json["extra"] = roomName;
+
+  return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json para notificar al cliente que aún no ha sido invitado
+ * a un cuarto en el que intenta hacer alguna petición. Las operaciones
+ * válidas para esta función son: ROOM_TEXT, ROOM_USERS, LEAVE_ROOM, INVITE.
+ * @param roomName el nombre del cuarto al que el cliente aún no ha sido
+ * invitado.
+ * @param type el tipo de operación válido para esta función.
+ * @return el json convertido a cadena.
+ * @throw InvalidOperationException si la función recibe una operación
+ * inválida.
+ */
 std::string JsonController::getNotJoinedResponse(std::string roomName, Operation::Type type){
-  return "";
+  Json::Value json;
+  std::string operation;
+
+  switch(type){
+  case Operation::Type::JOIN_ROOM:
+  case Operation::Type::TEXT: throw InvalidOperationException("");
+  default: break;
+  }
+
+  operation = Operation::getStringFromType(type);
+  
+  json["type"] = "RESPONSE";
+  json["operation"] = operation;
+  json["result"] = "NOT_JOINED";
+  json["extra"] = roomName;
+
+  return turnJsonToString(json);
 }
 
-std::string JsonController::getRoomUsersResponse(std::vector<Client> roomUsers){
-  return "";
+/**
+ * Se construye el json con la lista de clientes en un cuarto al que el
+ * usuario ya se encuentra unido.
+ * @param roomName el nombre del cuarto de cual se obtendrá la lista.
+ * @param roomUsers un vector con los clientes del cuarto.
+ * @return el json convertido a cadena.
+ */
+std::string JsonController::getRoomUsersResponse(std::string roomName, std::vector<Client> roomUsers){
+  Json::Value json;
+  Json::Value users;
+
+  for(Client c : roomUsers){
+
+    users[c.getUserName()] = c.getUserStatus();
+  }
+  
+  json["type"] = "ROOM_USER_LIST";
+  json["roomname"] = roomName;
+  json["users"] = users;
+
+  return turnJsonToString(json);
 }
 
 
-  /**Mensajes generales*/
+  /**Avisos generales*/
+
+/**
+ * Se construye el json que notifica a todos los clientes del chat que un
+ * nuevo usuario se ha identificado.
+ * @param username el nombre del usuario recien conectado.
+ * @return el json convertido a string.
+ */
 std::string JsonController::getNewUserAdvice(std::string username){
   Json::Value json;
 
@@ -149,6 +318,12 @@ std::string JsonController::getNewUserAdvice(std::string username){
 
 }
 
+/**
+ * Se construye el json que notifica a todos los clientes del chat que un
+ * cliente ha actualizado su status.
+ * @param client el cliente que actualizó su estado.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getNewStatusAdvice(Client client){
   Json::Value json;
 
@@ -159,6 +334,12 @@ std::string JsonController::getNewStatusAdvice(Client client){
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json con un mensaje para un cliente en específico.
+ * @param username el nombre del usuario que mandó un mensaje privado.
+ * @param message una cadena con el mensaje privado.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getPrivateTextAdvice(std::string username, std::string message){
   Json::Value json;
 
@@ -169,6 +350,12 @@ std::string JsonController::getPrivateTextAdvice(std::string username, std::stri
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json que contiene un mensaje general.
+ * @param username el nombre de usuario que envió el mensaje.
+ * @param message el mensaje público.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getPublicTextAdvice(std::string username, std::string message){
   
   Json::Value json;
@@ -179,18 +366,61 @@ std::string JsonController::getPublicTextAdvice(std::string username, std::strin
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json con una invitación de unión a un cuarto.
+ * @param username el nombre del usuario que envió la invitación.
+ * @param roomName el nombre del cuarto al que se está invitando.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getInvitationAdvice(std::string username, std::string roomName){
-  return "";
+  Json::Value json;
+  json["type"] = "INVITATION";
+  json["username"] = username;
+  json["roomname"] = roomName;
+
+  return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json que notifica a los clientes de un cuarto cuando un
+ * nuevo usuario se une.
+ * @param username el nombre del usuario que recien se unió.
+ * @param roomName el nombre del cuarto al que se unió el usuario.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getUserJoinedRoomAdvice(std::string username, std::string roomName){
-  return "";
+  Json::Value json;
+  json["type"] = "JOINED_ROOM";
+  json["username"] = username;
+  json["roomname"] = roomName;
+
+  return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json con el mensaje para un cuarto.
+ * @param username el nombre del ususario que envió el mensaje.
+ * @param roomName el nombre del cuarto que recibirá el mensaje.
+ * @param message el mensaje para el cuarto.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getRoomTextAdvice(std::string username, std::string roomName, std::string message){
-  return "";
+  Json::Value json;
+  json["type"] = "ROOM_TEXT_FROM";
+  json["roomname"] = roomName;
+  json["username"] = username;
+  json["text"] = message;
+
+  return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json que notifica a los clientes en un cuarto que alguien
+ * abandonó.
+ * @param username el nombre del usuario que salió de un cuarto.
+ * @param roomName el nombre del cuarto del que el usuario salió.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getLeftRoomAdvice(std::string username, std::string roomName){
 
   Json::Value json;
@@ -202,6 +432,12 @@ std::string JsonController::getLeftRoomAdvice(std::string username, std::string 
   return turnJsonToString(json);
 }
 
+/**
+ * Se construye el json que notifica a todos los clientes que un usuario
+ * abandonó el chat.
+ * @param username el nombre del usuario que abandonó el chat.
+ * @return el json convertido a cadena.
+ */
 std::string JsonController::getUserDisconnectedAdvice(std::string username){
 
   Json::Value json;
@@ -212,3 +448,12 @@ std::string JsonController::getUserDisconnectedAdvice(std::string username){
   return turnJsonToString(json);
 }
 
+/**
+ * Se convierte un objeto json a una cadena.
+ * @return output el json convertido a cadena.
+ */
+std::string JsonController::turnJsonToString(Json::Value json){
+  Json::StreamWriterBuilder builder;
+  const std::string output = Json::writeString(builder, json);
+  return output;
+}
--- server/src/main/json_controller.h
+++ server/src/main/json_controller.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include "client.h"
 #include "operation.h"
+#include "invalid_operation_exception.cpp"
 
 
 class JsonController{
@@ -16,39 +17,63 @@ class JsonController{
   JsonController();
 
   /**Errores de formato*/
+  // Crea un json para notificar un error de formato.
   std::string getInvalidJsonResponse();
+  // Crea un Json para notificar al cliente que aún no se identifica.
   std::string getNotIdentifiedResponse();
-  std::string getInvalidRoomNameResponse();
+  // Crea un json si el nombre del cuarto no tiene formato adecuado.
+  std::string getNewRoomMaxCharResponse(std::string roomName);
+  // Crea un json si el nombre de usuario no tiene formato adecuado.
+  std::string getUsernameMaxCharResponse(std::string username);
 
   /**Respuestas para el solicitante de la petición*/
+  // Crea el Json para la respuesta de identificacion.
   std::string getIdentificationResponse(std::string username);
+  // Crea el json que informa que el nombre de usuario ya existe.
   std::string getUsernameAlreadyExistsResponse(std::string username);
-  std::string getUsernameMaxCharResponse(std::string username);
+  // Crea el json con la lista de usuarios.
   std::string getServerUserListResponse(std::map<int, Client> usersMap);
+  // Crea el json de confirmación de creación de un cuarto.
   std::string getNewRoomCreatedResponse(std::string roomName);
-  std::string getNewRoomMaxCharResponse(std::string roomName);
-  std::string getRoomAlreadyExistsResponse(std::string roomName); 
-  std::string getNoSuchRoomResponse(std::string roomName, Operation::Type type); /**INVITE, JOIN_ROOM, ROOM_USERS, ROOM_TEXT, LEAVE_ROOM*/
-  std::string getNoSuchUserResponse(std::string username, Operation::Type type); /**TEXT, INVITE */
+  // Crea el json para notificar al cliente que el cuarto ya existe.
+  std::string getRoomAlreadyExistsResponse(std::string roomName);
+  // Crea el json que notifica al cliente que el cuarto no existe.
+  std::string getNoSuchRoomResponse(std::string roomName, Operation::Type type);
+  // Crea el json que notifica al cliente que un usuario no existe.
+  std::string getNoSuchUserResponse(std::string username, Operation::Type type); 
+  // Crea el json de confirmación de unión a un cuarto.
   std::string getJoinRoomSuccessResponse(std::string roomName);
-  std::string getNotInvitedResponse(std::string roomName, Operation::Type type); /**JOIN_ROOM, */
-  std::string getNotJoinedResponse(std::string roomName, Operation::Type type);/**ROOM_TEXT, ROOM_USERS, LEAVE_ROOM*/
-  std::string getRoomUsersResponse(std::vector<Client> roomUsers);
+  // Crea el json que notifica al usuario que no ha sido invitado a un cuarto.
+  std::string getNotInvitedResponse(std::string roomName);
+  // Crea el json que notifica al cliente que aún no se une al cuarto.
+  std::string getNotJoinedResponse(std::string roomName, Operation::Type type);
+  // Crea un json con la lista de clientes en un cuarto.
+  std::string getRoomUsersResponse(std::string roomName, std::vector<Client> roomUsers);
 
-  /**Mensajes generales*/
+  /**Avisos generales*/
+  // Crea el json que informa de la unión de un nuevo cliente.
   std::string getNewUserAdvice(std::string username);
+  // Crea el json que notifica el cambio de estado de un cliente.
   std::string getNewStatusAdvice(Client client);
+  // Crea el json con un mensaje privado.
   std::string getPrivateTextAdvice(std::string username, std::string message);
+  // Crea el json con un mensaje público.
   std::string getPublicTextAdvice(std::string username, std::string message);
+  // Crea el json que notifica a un cliente si fue invitado a un cuarto.
   std::string getInvitationAdvice(std::string username, std::string roomName);
+  // Crea el json que notifica de la unión de un cliente a un cuarto.
   std::string getUserJoinedRoomAdvice(std::string username, std::string roomName);
+  // Crea el json con un mensaje para un cuarto.
   std::string getRoomTextAdvice(std::string username, std::string roomName, std::string message);
+  // Crea el json que notifica a clientes de un cuarto si alguien se desconectó
   std::string getLeftRoomAdvice(std::string username, std::string roomName);
+  // Crea el json que avisa a los demás clientes que un cliente se desconectó.
   std::string getUserDisconnectedAdvice(std::string username);
   
   
 
  private:
+  // Convierte un json a cadena.
   std::string turnJsonToString(Json::Value json);
 };
 
