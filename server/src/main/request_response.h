6b209dfcdeaf2bc700944d48710913c7ff4d611c
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2025-07-21 21:52:15 -0600

    se documentó la clase request response

--- server/src/main/request_response.h
+++ server/src/main/request_response.h
@@ -9,6 +9,7 @@
 
 struct Message{
 
+  // Enumeración para el tipo de mensaje que puede haber.
   enum class MessageType{
     GENERAL = 1, // Se envia a todos los usuarios menos el emisor.
     SPECIFIC = 2, // Uso tanto para mensajes privados como mensajes a usuarios de algun cuarto
@@ -27,19 +28,24 @@ class RequestResponse{
   bool keepConection;
   // Respuesta para el usuario
   std::string userResponse;
-  // Aviso o mensaje para el cliente o clientes.
+  // Vector de mensajes para los demás clientes.
   std::vector<Message> messageVector;
   
  public:
   RequestResponse();
-  
+  // Regresa el vector de mensajes.
   std::vector<Message> getMessageVector();
+  // Regresa la respuesta del solicitante.
   std::string getUserResponse();
+  // Regresa el estado de la conexión.
   bool getKeepConection();
-
+  // Detiene la conexión del cliente.
   void stopConection();
+  // Se establece la respuesta del solicitante.
   void setUserResponse(std::string response);
+  // Agrega un nuevo mensaje general al vector de mensajes.
   void pushGeneralMessage(std::string json);
+  // Agrega un nuevo mensaje específico al vector de mensajes.
   void pushSpecificMessage(std::string json, std::vector<int> targetSockets);
   
 };
