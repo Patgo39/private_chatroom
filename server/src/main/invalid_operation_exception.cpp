6f4eade9fb0d5c8b162c32cfc7d7d58dcf9461f6
Patricio García Ocampo <patriciogarcia321@ciencias.unam.mx>
2025-07-21 22:19:44 -0600

    Se documentaron las excepciones

--- server/src/main/invalid_operation_exception.cpp
+++ server/src/main/invalid_operation_exception.cpp
@@ -1,3 +1,14 @@
+/**
+ * @class InvalidOperationException
+ * @brief Excepción lanzada cuando se recibe un objeto de tipo Operacion
+ *        en una función que no lo admite.
+ *
+ * Esta excepción se usa para indicar que un objeto Operacion ha sido 
+ * ingresado en un contexto donde no se espera ese tipo. Es útil para 
+ * detectar errores lógicos en tiempo de ejecución, cuando el tipo de 
+ * operación no es válido para la función o flujo actual.
+ */
+
 #include <exception>
 #include <iostream>
 #include <string>
